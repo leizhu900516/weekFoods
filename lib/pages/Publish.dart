37ddacdd0191bca0ca0adb85abede79f72f6e071
leizhu900516 <leizhu900516@163.com>
2019-05-04 09:23:57 +0800

    发布页修改

--- lib/pages/Publish.dart
+++ lib/pages/Publish.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:async';
+//import 'package:async/async.dart';
+
 
 class PublishFood extends StatefulWidget{
 
@@ -7,12 +11,115 @@ class PublishFood extends StatefulWidget{
 }
 
 class _PublishFoodState extends State<PublishFood>{
+  var  _name = '';
+  var _imgPath;
+  //定义表单实例
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _title = '';
+  String _content = '';
+
+
+  void _forSubmitted() {
+    var _form = _formKey.currentState;
+
+    if (_form.validate()) {
+      _form.save();
+      print("提交了");
+      print(_title);
+      print(_content);
+    }
+  }
+
+  /*图片控件*/
+  Widget _ImageView(imgPath) {
+    if (imgPath == null) {
+      return Center(
+        child: Text("请选择图片或拍照"),
+      );
+    } else {
+      return Container(
+        height: 100.0,
+        width: 100.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10.0))
+        ),
+        child: Image.file(
+          imgPath,
+        ),
+      );
+    }
+  }
+
+  void _uploadPhoto() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _imgPath = image;
+    });
+  }
+
+  void _selectPhoto() async{
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _imgPath = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      appBar: AppBar(
+        title: Text("发布新菜品"),
+      ),
       body: Container(
-        child: Text("发布"),
+        padding: EdgeInsets.all(10.0),
+        child:Form(
+            key: _formKey,
+            child: ListView(
+              children: <Widget>[
+                _ImageView(_imgPath),
+                Container(
+                  child: RaisedButton(onPressed: _uploadPhoto,child: Text("上传照片"),),
+                ),
+                Container(
+                  child: RaisedButton(onPressed: _selectPhoto,child: Text("选择照片"),),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "标题"),
+                  validator: (String value){
+                    if (value.isEmpty){
+                      return '标题不能为空';
+                    }
+                  },
+                  onSaved: (String title){
+                    _title = title;
+//                    print(title);
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "描述信息"),
+                  validator: (String value){
+                    if (value.isEmpty || value.length<20){
+                      return '描述至少20字';
+                    }
+                  },
+                  onSaved: (String content){
+                    _content = content;
+//                    print(content);
+                  },
+                ),
+                Container(
+                  child: RaisedButton(
+                    onPressed: _forSubmitted,
+                    child: Text("发布"),
+                    color: Colors.yellow,
+                  ),
+                )
+
+              ],
+            )
+        )
       ),
     );
   }
